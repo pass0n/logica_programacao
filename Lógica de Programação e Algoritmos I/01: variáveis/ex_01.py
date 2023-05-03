1cf3d73fc29d0b96ee741e2136f1146253364339
passon <kayllonsv.passon@gmail.com>
2023-05-03 14:57:09 -0300

    Lógica.I: variáveis

---
+++ Lógica de Programação e Algoritmos I/01: variáveis/ex_01.py
@@ -0,0 +1,8 @@
+# Algoritmo: Trabalhando com Tipos e Variáveis
+
+nome = input("Escreva o seu nome: ");
+sobrenome = input("Escreva o seu sobrenome: ");
+
+nomeCompleto = nome + " " + sobrenome;
+
+print(nomeCompleto);
